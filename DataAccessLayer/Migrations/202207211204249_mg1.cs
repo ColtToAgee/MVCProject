bddf404119b9b98f245e60a68e7b186832dabcbf
cozgonul-stj <cozgonul-stj@COZGONUL-STJ>
2022-07-21 16:54:01 +0300

    Add project files.

---
+++ DataAccessLayer/Migrations/202207211204249_mg1.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mg1 : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Contents", name: "Writer_WriterID", newName: "WriterId");
+            RenameIndex(table: "dbo.Contents", name: "IX_Writer_WriterID", newName: "IX_WriterId");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.Contents", name: "IX_WriterId", newName: "IX_Writer_WriterID");
+            RenameColumn(table: "dbo.Contents", name: "WriterId", newName: "Writer_WriterID");
+        }
+    }
+}
